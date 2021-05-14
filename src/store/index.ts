ef302641735b12349f2129d48a7c77c06de67430
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-05-14 08:45:02 -0300

    tipagens do Redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,11 @@
 //estado global do redux
 import { createStore } from 'redux'
 import rootReduce from './modules/rootReduce'
+import { ICartState } from './modules/cart/types'
+
+export interface IState {
+  cart: ICartState
+}
 
 const store = createStore(rootReduce) //valor inicial do store
 
