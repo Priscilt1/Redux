217668f0ce05066deef56f5a4f702ef5494eb9ff
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-05-14 08:58:05 -0300

    Usando o Immer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,6 +1,7 @@
 //o reduce determina quais informações estão contidas dentro do modulo
 //retorna os dadoscontidos dentro do estado
 import { Reducer } from 'redux'
+import produce from 'immer'
 import { ICartState } from './types'
 
 const INITTIAL_STATE: ICartState = { 
@@ -8,25 +9,23 @@ const INITTIAL_STATE: ICartState = {
 }
 
 const cart: Reducer<ICartState> = (state = INITTIAL_STATE, action) => {
+  return produce(state, draft => { //draft - rascunho
   switch(action.type) {
-    case 'ADD_PRODUCT_TO_CART': {
-      const { product } = action.payload
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload
 
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
+          draft.items.push({ //atualizando o carrinho
             product,
             quantity: 1,
-          }
-        ]
+          }) 
+
+        break
+      }
+      default: {
+        return draft
       }
     }
-    default: {
-      return state
-    }
-  }
+  })
 }
 
 export default cart
